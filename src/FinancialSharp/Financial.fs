3ec26e574c97eedea156aff9fa5788cab80c5f0f
Enrico Galassi <enrico.galassi.88@gmail.com>
2020-07-24 23:58:13 +0200

    Implement function pmt

--- src/FinancialSharp/Financial.fs
+++ src/FinancialSharp/Financial.fs
@@ -22,3 +22,9 @@ type Financial =
     static member FV(rate : double, nper : double, pmt : double, pv : double) =
         Financial.FV(rate, nper, pmt, pv, PaymentDuePeriod.End)
     
+    static member PMT(rate: double, nper: double, pv: double, fv : double, paymentDuePeriod : PaymentDuePeriod) =
+        let temp = (1.0 + rate) ** nper
+        let paymentDuePeriodMult = match paymentDuePeriod with | Begin -> 1.0 | End -> 0.0
+        let maskedRate = match rate with | 0.0 -> 1.0 | _ -> rate
+        let fact = match rate with | 0.0 -> (1.0 + maskedRate * paymentDuePeriodMult) * (temp - 1.0) / maskedRate | _ -> nper
+        -(fv + pv * temp) / fact
