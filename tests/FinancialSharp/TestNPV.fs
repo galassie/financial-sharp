8a3941c3fbaab05f74a3e1d3c1365d741222fc8b
Enrico Galassi <enrico.galassi.88@gmail.com>
2021-02-11 17:46:13 +0100

    Add test to npv function

---
+++ tests/FinancialSharp/TestNPV.fs
@@ -0,0 +1,12 @@
+﻿namespace FinancialSharp.Tests
+
+open NUnit.Framework
+open FinancialSharp
+
+[<TestFixture>]
+type TestNPV() = 
+    
+    [<Test>]
+    member _.``npv``() = 
+        let result = Financial.NPV(0.05, [|-15000.0; 1500.0; 2500.0; 3500.0; 4500.0; 6000.0|])
+        Assert.AreEqual(122.89485495093959, result)
