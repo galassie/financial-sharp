44f8bfdb915b495b432f9a4acc292ec47e6152bd
Enrico Galassi <enrico.galassi.88@gmail.com>
2021-02-18 23:09:06 +0100

    Add rate function and tests

--- src/FinancialSharp/Financial.fs
+++ src/FinancialSharp/Financial.fs
@@ -19,7 +19,7 @@ type Financial =
         else
             let paymentDuePeriod = defaultArg paymentDuePeriod0 PaymentDuePeriod.End
             let temp = (1.0 + rate) ** nper
-            (-pv * temp - pmt * (1.0 + rate * Financial.PaymentDuePeriodMult(paymentDuePeriod)) / rate * (temp - 1.0))
+            (-pv * temp - pmt * (1.0 + rate * (Financial.PaymentDuePeriodMult paymentDuePeriod)) / rate * (temp - 1.0))
 
     /// Compute the payment against loan principal plus interest
     static member PMT(rate:double, nper:double, pv:double, ?fv0:double, ?paymentDuePeriod0:PaymentDuePeriod) =
@@ -35,7 +35,7 @@ type Financial =
                 nper
             else
                 let paymentDuePeriod = defaultArg paymentDuePeriod0 PaymentDuePeriod.End
-                (1.0 + maskedRate * Financial.PaymentDuePeriodMult(paymentDuePeriod)) * (temp - 1.0) / maskedRate
+                (1.0 + maskedRate * (Financial.PaymentDuePeriodMult paymentDuePeriod)) * (temp - 1.0) / maskedRate
         -(fv + pv * temp) / fact
 
     /// Compute the number of periodic payments
@@ -45,7 +45,7 @@ type Financial =
             -(fv + pv) / pmt 
         else
             let paymentDuePeriod = defaultArg paymentDuePeriod0 PaymentDuePeriod.End
-            let z = pmt * (1.0 + rate * Financial.PaymentDuePeriodMult(paymentDuePeriod)) / rate
+            let z = pmt * (1.0 + rate * (Financial.PaymentDuePeriodMult paymentDuePeriod)) / rate
             Math.Log((-fv + z) / (pv + z)) / Math.Log(1.0 + rate)
 
     /// Compute the interest portion of a payment
@@ -83,8 +83,37 @@ type Financial =
                 nper
             else
                 let paymentDuePeriod = defaultArg paymentDuePeriod0 PaymentDuePeriod.End
-                (1.0 + rate * Financial.PaymentDuePeriodMult(paymentDuePeriod)) * (temp - 1.0) / rate
+                (1.0 + rate * (Financial.PaymentDuePeriodMult paymentDuePeriod)) * (temp - 1.0) / rate
         -(fv + pmt * fact) / temp
+
+    static member RATE(nper:double, pmt:double, pv:double, fv:double, ?paymentDuePeriod0:PaymentDuePeriod, ?guess0:double, ?tol0:double, ?maxiter0:int) =
+        let paymentDuePeriod = defaultArg paymentDuePeriod0 PaymentDuePeriod.End
+        let guess = defaultArg guess0 0.1
+        let tol = defaultArg tol0 1e-6
+        let maxiter = defaultArg maxiter0 100
+
+        let mutable rn = guess
+        let mutable iterator = 0
+        let mutable close = false
+
+        let g_div_gp r n p x y w =
+            let t1 = (r+1.0) ** n
+            let t2 = (r+1.0) ** (n-1.0)
+            let g = y + t1*x + p*(t1-1.0)*(r*w+1.0) / r
+            let gp = n*t2*x - p*(t1-1.0)*(r*w + 1.0) / (r ** 2.0) + n*p*t2 * (r*w + 1.0) / r + p*(t1-1.0)*w/r
+            g / gp
+
+        while (iterator < maxiter) && (not close) do
+            let rnp1 = rn - g_div_gp rn nper pmt pv fv (Financial.PaymentDuePeriodMult paymentDuePeriod)
+            let diff = Math.Abs(rnp1 - rn)
+            close <- diff < tol
+            iterator <- iterator + 1
+            rn <- rnp1
+        
+        if close then
+            Some rn
+        else
+            None
 
     /// Compute the NPV (Net Present Value) of a cash flow series
     static member NPV(rate:double, values:double seq) =
